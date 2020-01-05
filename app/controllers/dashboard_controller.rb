e4b8780e16b5bffaa1c6b6d6b6ed8bc095cb31fe
Ruturaj <ruturaj@venturit.com>
2020-01-05 13:53:16 +0530

    Integrated google cloud vision api into project

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
   def index
+    # GoogleVision.detect_text(ActionController::Base.helpers.image_path('sign_text.png'))
+
+    GoogleVision.detect_text_gcs('gs://cloud-samples-data/vision/ocr/sign.jpg')
   end
 end
